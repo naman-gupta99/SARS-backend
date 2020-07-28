c3ec2730192ad061b4aa3c77d0a7e95ac18789c2
naman-gupta99 <kungfunaman@gmail.com>
2020-07-28 17:46:37 +0530

    Clean Slate

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,3 @@
-import flightPlanRouter from "./routes/flight-plan";
-import flightStateRouter from "./routes/flight-state";
-
-const router = (app) => {
-  app.use("/flight-plan", flightPlanRouter);
-  app.use("/flight-state", flightStateRouter);
-  console.log("Routers set up ...");
-};
+const router = (app) => {};
 
 export default router;
