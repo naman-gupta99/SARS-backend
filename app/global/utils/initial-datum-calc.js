5d9bbcf5250decff28d6663adbd249ff6057e945
nandinigoel10 <nandini.goel@gmail.com>
2020-08-01 11:24:04 +0530

    Added Logic for Error Calculation

--- app/global/utils/initial-datum-calc.js
+++ app/global/utils/initial-datum-calc.js
@@ -1,3 +1,75 @@
+// ---- Get Radius/Width of Datum
+const meansOfNavErr = {
+  GNSS: 0.1,
+  Radar: 1,
+  Visual_fix: 1,
+  Celestial_fix: 2,
+  Marine_radio_beacon: 4,
+  LORAN_C: 1,
+  INS: 0.5,
+  VOR: 0.5,
+  TACAN: 0.5,
+};
+
+const craftError = {
+  Ship: 5,
+  Submarine: 5,
+  Aircraft_3: 5,
+  Aircraft_2: 10,
+  Aircraft_1: 15,
+  Submersible: 15,
+  Boat: 15,
+};
+
+const craftDRError = {
+  Ship: 1.05,
+  Submarine: 1.05,
+  Aircraft_3: 1.05,
+  Aircraft_2: 1.1,
+  Aircraft_1: 1.15,
+  Submersible: 1.15,
+  Boat: 1.15,
+};
+
+const evalNonDRError = (req) => {
+  if (req.meansOfNavigation == "Unknown") {
+    return craftError[req.typeOfCraft];
+  } else {
+    return meansOfNavErr[req.typeOfCraft];
+  }
+};
+
+const evalDRError = (req) => {
+  if (req.meansOfNavigation == "Unknown") {
+    return craftDRError[req.typeOfCraft] * craftError[req.typeOfCraft];
+  } else {
+    return craftDRError[req.typeOfCraft] * meansOfNavErr[req.meansOfNavigation];
+  }
+};
+
+const getCraftError = (req) => {
+  // --- Distressed Craft Error
+  const X = evalDRError(req.distressedCraft);
+
+  // --- Search Craft Error
+  let Y = 0;
+  if (req.searchFacilityUsesDRNav) {
+    Y = evalDRError(req.searchCraft);
+  } else {
+    Y = evalNonDRError(req.searchCraft);
+  }
+
+  return {
+    X: X,
+    Y: Y,
+  };
+};
+
+const getDriftError = (lkpTime) => {
+  return (+new Date() - lkpTime) / 7200000;
+};
+
+// ---- Get Latitude of Datum
 const getProjectileDistance = (alt, v_x, v_y) => {
   const t2 = Math.pow(v_y, 2) + 2 * 9.8 * alt;
   const t1 = Math.sqrt(t2);
@@ -54,7 +126,12 @@ const initialDatumCalc = (req) => {
     result.circle.longitude = pointOfImpact.longitude;
     result.circle.latitude = pointOfImpact.latitude;
 
-    result.circle.radius = 10000;
+    const craftErrors = getCraftError(req);
+    result.circle.x = craftErrors.X;
+    result.circle.y = craftErrors.Y;
+
+    const driftError = getDriftError(req.timestampLKP);
+    result.circle.driftError = driftError;
     result.circle.trustVal = 75;
   } else {
     // Line Datum
@@ -64,11 +141,16 @@ const initialDatumCalc = (req) => {
     result.line.sourceLongitude = pointOfImpact.longitude;
     result.line.sourceLatitude = pointOfImpact.latitude;
 
-    result.line.destinationLongitude = req.destinationLongitude;
-    result.line.destinationLatitude = req.destinationLatitude;
+    result.line.destinationLongitude = req.socScenario.longitude;
+    result.line.destinationLatitude = req.socScenario.latitude;
 
-    result.width = 10000;
-    result.trustVal = 75;
+    const craftErrors = getCraftError(req);
+    result.line.x = craftErrors.X;
+    result.line.y = craftErrors.Y;
+
+    const driftError = getDriftError(req.timestampLKP);
+    result.line.driftError = driftError;
+    result.line.trustVal = 75;
   }
 
   return result;
