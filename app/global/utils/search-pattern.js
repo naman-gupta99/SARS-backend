0cfb54deaeae1af33f9a006ac1d698426f5e7355
Tamanna210 <tamannasain21@gmail.com>
2020-08-02 22:33:55 +0530

    merged coverage parameter

--- app/global/utils/search-pattern.js
+++ app/global/utils/search-pattern.js
@@ -1,46 +1,25 @@
-// if(terrain=="mountain")
-//   return contour;
-// else if(terrain=="sea")
-//   return shoreline;
-// else if(radius_large)              //20 in Nautical Miles
-// {
-//     if(location_known_Accuracy)
-//       return trackline;
-//     else 
-//       return parallelSweep;     //creepingLine less efficient
-// }
-// else
-// {
-//     if(uniform_coverage)
-//        return expandingSquare;
-//     return sector;
-// }
-
 const searchPatterns = (req) => {
     const sector = {
         name: "Sector Search",
-        description: "small Area Circular search intensive coverage at center",
+        description: "small Area Circular search when location is known with good accuracy",
         location_known_Accuracy: true,
         radius_large: false,
-        uniform_coverage: false,
         terrain: "flat"
    };
    
    const expandingSquare = {
        Name: "Expanding Square Search",
-       description: "small area rectangular search with unifrom coverage",
+       description: "small area rectangular search with uniform coverage",
        location_known_Accuracy: true,
        radius_large: false,
-       uniform_coverage: true,
        terrain: "flat"
    };
    
    const trackline = {
        Name: "Trackline Search",
-       description: "large area Track search with used for first effort search",
+       description: "large area Track search used for first effort search",
        location_known_Accuracy: true,
        radius_large: true,
-       uniform_coverage: true,
        terrain: "flat"
    };
    
@@ -49,34 +28,30 @@ const searchPatterns = (req) => {
        description: "large area search with parallel sweeps",
        location_known_Accuracy: false,
        radius_large: true,
-       uniform_coverage: true,
        terrain: "flat"
    };
    
    const creepingLine = {
        Name: "Creeping Line Search",
-       description: "A large area search with parallen sweeps in rectangle",
+       description: "A large area search with parallel sweeps in rectangle",
        location_known_Accuracy: false,
        radius_large: true,
-       uniform_coverage: true,
        terrain: "flat"
    };
    
    const contour = {
        Name: "Contour Search",
-       description: "This Mountain terrain search circles mountain from top to bottom",
+       description: "This Mountain terrain search, circles mountain from top to bottom",
        location_known_Accuracy: false,
        radius_large: true,
-       uniform_coverage: true,
        terrain: "mountain"
    };
    
    const shoreline = {
        Name: "Shoreline Search",
-       description: "Search at survivor at dry land near sea, clinging to rocks of sea and in boats sea ",
+       description: "Search survivors at dry land near sea, clinging to rocks of sea and in boats in sea",
        location_known_Accuracy: false,
        radius_large: true,
-       uniform_coverage: true,
        terrain: "sea"
    };
 
@@ -96,12 +71,7 @@ const searchPatterns = (req) => {
         }
     }
     else{
-        if(uniform_coverage){
-            search_patterns.filter(function(element){ return (!radius_large)&&uniform_coverage});           
-        }
-        else{
-            search_patterns.filter(function(element){ return (!radius_large)&&(!uniform_coverage)});
-        }
+        search_patterns.filter(function(element){ return (!radius_large)});           
     }
 };
 
