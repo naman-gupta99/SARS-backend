f791617d1920166bee09cb918a49944127781d14
naman-gupta99 <kungfunaman@gmail.com>
2020-08-03 13:31:19 +0530

    Geocoding API added

--- app/global/utils/reverse-geocoder.js
+++ app/global/utils/reverse-geocoder.js
@@ -1,27 +1,27 @@
 import request from "request";
 import config from "../../../config";
+
 const reverseGeo = (lat, lon) => {
-    return new Promise((resolve, reject) => {
-	let uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
-	uri = uri+lat+","+lon+"&key="+config.google.apiKey;
-	// console.log(uri);
-	request.get(uri, (err, res, body) => {
-	    if (err) {
-		console.log(err);
-		resolve("err");
-	    } else {
-		body = JSON.parse(body);
-		//console.log(body);
-		resolve(body.results[0].formatted_address);
-		
-	    }
-	});
+  return new Promise((resolve, reject) => {
+    let uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+    uri = uri + lat + "," + lon + "&key=" + config.google.apiKey;
+    // console.log(uri);
+    request.get(uri, (err, res, body) => {
+      if (err) {
+        console.log(err);
+        resolve("err");
+      } else {
+        body = JSON.parse(body);
+        //console.log(body);
+        resolve(body.results[0].formatted_address);
+      }
     });
+  });
 };
 const getAddress = async (lat, lon) => {
-    const x = await reverseGeo(lat, lon);
-    // console.log(x);
-    return x;
+  const x = await reverseGeo(lat, lon);
+  // console.log(x);
+  return x;
 };
 
 // getAddress(28.0, 77,0);
--- app/routes/geo-info.js
+++ app/routes/geo-info.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ResponseTemplate from "../global/templates/response";
 import contactSearch from "../global/utils/contact-search";
+import getAddress from "../global/utils/reverse-geocoder";
 
 const router = express.Router();
 
@@ -16,6 +17,20 @@ router.get("/get-contacts/:longitude/:latitude", async (req, res) => {
   res
     .status(200)
     .json(ResponseTemplate.success("Contacts Found", contactDetails));
+});
+
+// @route  GET geo-info/get-location
+// @desc   Get current location
+// @access Private
+router.get("/get-location/:longitude/:latitude", async (req, res) => {
+  const locationDetails = await getAddress(
+    req.params.latitude,
+    req.params.longitude
+  );
+
+  res
+    .status(200)
+    .json(ResponseTemplate.success("Location Found", locationDetails));
 });
 
 export default router;
